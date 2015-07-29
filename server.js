6c5c1866dfbaacc1397603d4b07266aad2a839f2
NBram <nikolasbram@gmail.com>
2015-07-29 16:22:41 +0200

    server.js working - http request to auth-server added

--- server.js
+++ server.js
@@ -8,9 +8,10 @@
 	var app        	= express();                 // define our app using express
 	var bodyParser 	= require('body-parser');
 	var http 		= require('http');
+	var querystring = require("querystring");
 
 
-	var accessToken = 'token';
+	var accessToken = 'ii9hD7yw8ao9ereDh34aer93db';
 	var receivedAccessToken;
 
 	// configure app to use bodyParser()
@@ -26,58 +27,49 @@
 
 	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 	router.get('/', function(req, res) {
-	    res.json({ message: 'Welcome to the Resource Server' });   
+	    res.json({ message: 'Welcome to the Resource Server' }); 
 	});
 
 
-	// Try to access data 
+	// token received from client 
 	router.post('/token', function(req, res) {
+		// Coordination with Authoriazation server?
+		receivedAccessToken = req.body.accessToken;
 
-		receivedAccessToken = req.body.token;
-		// Coordination with Authoriazation server? 
+		var data = querystring.stringify({data: 'Some data'});
+		 
+		var url1 = "pi-auth-server.herokuapp.com/token-validation?token="+receivedAccessToken;
+		var url = "pi-auth-server.herokuapp.com";
 
 		var options = {
-		    host: 'pi-auth-server.herokuapp.com',
-		    port: 80,
-		    path: '/token-authentication',
-		    method: 'POST',
-		    headers: {
-		      'Content-Type': 'application/x-www-form-urlencoded',
-		      'Content-Length': Buffer.byteLength(receivedAccessToken)
-		    },
-	  	};
+			host: url,
+			port: 80,
+			method: 'GET',
+			path: '/token-validation?token='+receivedAccessToken,
+    		headers: {
+        		'Content-Type': 'application/x-www-form-urlencoded',
+        		'Content-Length': Buffer.byteLength(data)
+    		}
+		};
 
-	  	var req = http.request(options, function(res){
-        	res.setEncoding('utf8');
-        	res.on('receivedAccessToken', function (chunk) {
-            	console.log("body: " + chunk);
-        	});
-    	});
-    	req.write(receivedAccessToken);
-    	req.end();
+		var requ = http.request(options, function(resp) {
+	    	resp.setEncoding('utf8');
+	    	if(resp.statusCode == '200'){
+				res.json({ message: 'Access authorized',
+						   data: {
+						   		name: 'Bob',
+						   		age: '30',
+						   		gender: 'male'
+						   }}); 
+	    	} else {
+				res.json({ message: 'Access denied!' }); 
+	    	}
+		});
 
-
-
-		if (req.body.accessToken === accessToken) {
-			
-			
-
-
-
-			res.json({ message: 'Access authorized', data: 'some data'}); 
-
-
-		}
-		else {
-			res.json({ message: 'Access denied!' }); 
-		}	
-	})
-
-
-	router.post('/Coordination', function(req, res){
+		requ.write(data);
+		requ.end();
 
 	});
-
 
 	// more routes for our API will happen here
 
